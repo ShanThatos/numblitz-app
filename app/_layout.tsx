1801b86324bbb73cd7ffb4bf1882d36469bc7901
ShanThatos <shanthkoka@gmail.com>
2023-10-17 10:23:23 -0500

    nativewind bugfixes, setup complete?

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,9 @@
 import { Slot } from "expo-router"
+import { NativeWindStyleSheet } from "nativewind"
 
 import useLoadedAssets from "../hooks/setup/setup"
+
+NativeWindStyleSheet.setOutput({ default: "native" })
 
 export default function App() {
   const [assetsLoaded] = useLoadedAssets()
