01a55be59022c091407e5bff3f66c60a88d90d00
ShanThatos <shanthkoka@gmail.com>
2023-11-15 13:27:41 -0600

    finished quiz flow

--- app/home/practice/[category]/[model]/quiz.tsx
+++ app/home/practice/[category]/[model]/quiz.tsx
@@ -1,18 +1,22 @@
-import { useFocusEffect, useLocalSearchParams } from "expo-router"
-import { useCallback, useState } from "react"
+import { router, useFocusEffect, useLocalSearchParams } from "expo-router"
+import { useCallback, useMemo, useState } from "react"
 
 import {
+  FlexColCenter,
   FlexRowCenter,
   FullFlexColCenter,
   FullView,
+  Header2,
   Header3,
   Header4,
   Header5,
   Pressable,
+  Text,
+  View,
 } from "../../../../../components/base"
 import QuizScreen from "../../../../../components/quiz/QuizScreen"
 import { useModel } from "../../../../../hooks/requests/models"
-import { authFetchGet } from "../../../../../utils/Query"
+import { authFetchGet, authFetchPost } from "../../../../../utils/Query"
 
 export interface MathProblem {
   question: string
@@ -28,6 +32,8 @@ const useModelQuiz = (modelId: string) => {
   const [problems, setProblems] = useState<MathProblem[]>([])
   const [current, setCurrent] = useState(0)
   const [submissions, setSubmissions] = useState<string[]>([])
+  const [quizStartTime, setQuizStartTime] = useState(0)
+  const [averageTime, setAverageTime] = useState(0)
 
   const getProblems = useCallback(
     async () =>
@@ -46,6 +52,7 @@ const useModelQuiz = (modelId: string) => {
 
   const startQuiz = useCallback(() => {
     setQuizState("started")
+    setQuizStartTime(Date.now())
   }, [])
 
   const updateProblems = useCallback(async () => {
@@ -54,28 +61,63 @@ const useModelQuiz = (modelId: string) => {
     setProblems((prev) => [...prev, ...newProblems])
   }, [problems, current, getProblems])
 
-  const getScore = useCallback(() => {
-    let score = 0
-    for (let i = 0; i < submissions.length; i++) {
-      if (submissions[i] === problems[i].answer) score += 5
-      else score = Math.max(score - 4, 0)
-    }
-    return score
-  }, [submissions])
+  const getScore = useCallback(
+    (submissions: string[], problems: MathProblem[]) => {
+      let score = 0
+      for (let i = 0; i < submissions.length; i++) {
+        if (submissions[i] === problems[i].answer) score += 5
+        else score = Math.max(score - 4, 0)
+      }
+      return score
+    },
+    [],
+  )
+
+  const finishQuiz = useCallback(
+    async (finalSubmissions: string[]) => {
+      setQuizState("finished")
+      const averageTime =
+        (Date.now() - quizStartTime) / 1000 / finalSubmissions.length
+      setAverageTime(averageTime)
+      const { progress } = await authFetchGet(
+        `/mathgen/modelprogress/${modelId}`,
+      )
+      authFetchPost(`/mathgen/modelprogress/${modelId}`, {
+        params: {
+          progress: progress + 1,
+          average_time: averageTime.toFixed(1),
+        },
+      })
+    },
+    [quizStartTime, modelId],
+  )
 
   const submitAnswer = useCallback(
     (answer: string) => {
       const isCorrect = answer === problems[current].answer
-      setSubmissions((prev) => [...prev, answer])
-      const score = getScore()
-      if (score >= 50) setQuizState("finished")
+      const newSubmissions = [...submissions, answer]
+      setSubmissions(newSubmissions)
+      const score = getScore(newSubmissions, problems)
+      if (score >= 50) finishQuiz(newSubmissions)
       else {
         setCurrent((prev) => prev + 1)
         updateProblems()
       }
       return isCorrect
     },
-    [problems, current, getScore, updateProblems],
+    [problems, submissions, current, getScore, updateProblems, finishQuiz],
+  )
+
+  const numCorrect = useMemo(() => {
+    let numCorrect = 0
+    for (let i = 0; i < submissions.length; i++)
+      if (submissions[i] === problems[i].answer) numCorrect++
+    return numCorrect
+  }, [submissions, problems])
+
+  const numIncorrect = useMemo(
+    () => submissions.length - numCorrect,
+    [submissions, numCorrect],
   )
 
   return {
@@ -88,11 +130,15 @@ const useModelQuiz = (modelId: string) => {
     updateProblems,
     startQuiz,
     submitAnswer,
+    numCorrect,
+    numIncorrect,
+    averageTime,
   }
 }
 
 export default function ModelQuizScreen() {
-  const { model: modelId } = useLocalSearchParams<{
+  const { category: categoryId, model: modelId } = useLocalSearchParams<{
+    category: string
     model: string
   }>()
 
@@ -104,6 +150,9 @@ export default function ModelQuizScreen() {
     refreshQuiz,
     startQuiz,
     submitAnswer,
+    numCorrect,
+    numIncorrect,
+    averageTime,
   } = useModelQuiz(modelId || "")
 
   useFocusEffect(
@@ -112,12 +161,11 @@ export default function ModelQuizScreen() {
     }, []),
   )
 
-  if (!model || current >= problems.length)
-    return <FullView className="bg-primary" />
+  if (!model || current >= problems.length) return <FullView />
 
   if (state === "ready")
     return (
-      <FullFlexColCenter className="bg-primary">
+      <FullFlexColCenter>
         <Header3>{model.name}</Header3>
         <Header4>Practice Quiz</Header4>
         <FlexRowCenter className="mt-5">
@@ -139,6 +187,44 @@ export default function ModelQuizScreen() {
         answer={problems[current].answer}
       />
     )
-  else if (state === "finished") return <></>
+  else if (state === "finished")
+    return (
+      <FullView>
+        <FullFlexColCenter>
+          <Header2 className="pb-8">Done!</Header2>
+          <FlexRowCenter className="items-start" style={{ gap: 15 }}>
+            <FlexColCenter>
+              <FlexColCenter className="aspect-square w-16 rounded-full border-4 border-icon-darker">
+                <Text className="text-lg font-bold">
+                  {((numCorrect / (numCorrect + numIncorrect)) * 100).toFixed(
+                    0,
+                  )}
+                  %
+                </Text>
+              </FlexColCenter>
+              <Text className="text-lg font-bold">Accuracy</Text>
+            </FlexColCenter>
+            <FlexColCenter>
+              <FlexColCenter className="aspect-square w-16 rounded-full border-4 border-icon-darker">
+                <Text className="text-lg font-bold">
+                  {averageTime.toFixed(1)}s
+                </Text>
+              </FlexColCenter>
+              <Text className="text-center text-lg font-bold">Average</Text>
+            </FlexColCenter>
+          </FlexRowCenter>
+        </FullFlexColCenter>
+        <View className="px-4 pb-4">
+          <Pressable
+            className="rounded bg-icon p-2"
+            onPress={() => {
+              router.push(`/home/practice/${categoryId}/${modelId}`)
+            }}
+          >
+            <Header5 className="text-center text-white">Continue</Header5>
+          </Pressable>
+        </View>
+      </FullView>
+    )
   return <></>
 }
