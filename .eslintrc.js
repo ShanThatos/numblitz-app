5863c5b74f8d7797a02cf23f0bb0996bf918e651
ShanThatos <shanthkoka@gmail.com>
2024-08-28 18:11:30 -0500

    added nw and rnr

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  extends: [
+    "expo",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/strict-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/self-closing-comp": "error",
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "variableLike",
+        format: ["camelCase", "UPPER_CASE", "PascalCase"],
+      },
+      {
+        selector: "parameter",
+        format: ["camelCase"],
+        leadingUnderscore: "allow",
+      },
+      {
+        selector: "typeLike",
+        format: ["PascalCase"],
+      },
+    ],
+  },
+  root: true,
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+https://docs.expo.dev/more/create-expo/
+https://www.nativewind.dev/v4/overview
+https://rnr-docs.vercel.app/getting-started/introduction/
